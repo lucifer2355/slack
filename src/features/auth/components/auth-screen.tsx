6ad9e3ee4ad33ed3c4ff639b8a1dad4790a3f336
Dhruvil <dgajjar999@gmail.com>
2024-09-17 23:39:22 +0530

    feat: Add SignIn & SignUp Card

--- src/features/auth/components/auth-screen.tsx
+++ src/features/auth/components/auth-screen.tsx
@@ -7,8 +7,8 @@ export const AuthScreen = () => {
   const [state, setState] = useState<SignInFlow>("signIn");
 
   return (
-    <div>
-      <h1>Auth Screen</h1>
+    <div className='h-full flex items-center justify-center bg-[#5c3b58]'>
+      <div className='md:h-auto md:w-[26.25rem]'>Auth Screen</div>
     </div>
   );
 };

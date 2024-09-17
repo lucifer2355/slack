cfc3dad13bf8d6f32baa1b046808439dd4daa55a
Dhruvil <dgajjar999@gmail.com>
2024-09-17 23:42:17 +0530

    feat: Card, Input & Separater Component

--- src/features/auth/components/auth-screen.tsx
+++ src/features/auth/components/auth-screen.tsx
@@ -2,13 +2,17 @@
 
 import { useState } from "react";
 import { SignInFlow } from "../types";
+import { SignInCard } from "./sign-in-card";
+import { SignUpCard } from "./sign-up-card";
 
 export const AuthScreen = () => {
   const [state, setState] = useState<SignInFlow>("signIn");
 
   return (
     <div className='h-full flex items-center justify-center bg-[#5c3b58]'>
-      <div className='md:h-auto md:w-[26.25rem]'>Auth Screen</div>
+      <div className='md:h-auto md:w-[26.25rem]'>
+        {state === "signIn" ? <SignInCard /> : <SignUpCard />}
+      </div>
     </div>
   );
 };

d6c4e8235b55d53fd1e6a1e7057fd21069e7f2d3
Dhruvil <dgajjar999@gmail.com>
2024-09-17 23:59:02 +0530

    feat: Add Signin With Google & Github Option

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,3 +1,4 @@
+import { FcGoogle } from "react-icons/fc";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -7,16 +8,18 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { FaGithub } from "react-icons/fa";
 
 export const SignInCard = () => {
   return (
     <Card className='w-full h-full p-8'>
       <CardHeader className='px-0 pt-0'>
         <CardTitle>Login to continue</CardTitle>
+        <CardDescription>
+          Use your email or another service to continue
+        </CardDescription>
       </CardHeader>
-      <CardDescription>
-        Use your email or another service to continue
-      </CardDescription>
       <CardContent className='space-y-5 px-0 pb-0'>
         <form className='space-y-2.5'>
           <Input
@@ -39,6 +42,29 @@ export const SignInCard = () => {
             Continue
           </Button>
         </form>
+        <Separator />
+        <div className='flex flex-col gap-y-2.5'>
+          <Button
+            disabled={false}
+            onClick={() => {}}
+            className='w-full relative'
+            size='lg'
+            variant='outline'
+          >
+            <FcGoogle className='size-5 absolute top-2.5 left-2.5' />
+            Continue with Google
+          </Button>
+          <Button
+            disabled={false}
+            onClick={() => {}}
+            className='w-full relative'
+            size='lg'
+            variant='outline'
+          >
+            <FaGithub className='size-5 absolute top-2.5 left-2.5' />
+            Continue with Github
+          </Button>
+        </div>
       </CardContent>
     </Card>
   );
